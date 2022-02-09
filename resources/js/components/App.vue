f62c8686d0c711474ea868546c478812d5fd6334
salvosanzone <salvosansone.ss@gmail.com>
2022-02-09 16:54:40 +0100

    creazione componenti header e footer

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,17 +1,24 @@
 <template>
 
   <div>
-    <h1>home vue</h1>
+    <Header />
+    <Footer />
   </div>
   
 </template>
 
 <script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
 export default {
   name: "App",
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <header>
+    <nav>
+      <ul>
+      <li>
+        <a href="">Home</a> 
+        <a class="active" href="">Blog</a> 
+        <a href="">Chi siamo</a> 
+        <a href="">Contatti</a>
+      </li>
+    </ul>
+    </nav>
+    
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+  background-color: cadetblue;
+  text-align: center;
+  padding: 20px 0;
+
+  ul{
+    list-style: none;
+    li{
+      a{
+        margin: 0 10px;
+         text-decoration: none;
+         color: white;
+        &:hover{
+          text-decoration: underline;
+        } 
+        &.active{
+          color: crimson;
+        }
+      }
+    }
+  }
+}
+</style>
